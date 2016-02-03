8a00ccd79aeee9c8f31f90be4413f20346b5946c
Steven Harradine <stevenharradine@gmail.com>
2016-02-02 22:30:20 -0500

    clean up styles

--- cloner.js
+++ cloner.js
@@ -1,11 +1,11 @@
-var CONFIG = require("./config");
-var git_tools = require("./common");
+var CONFIG    = require("./config"),
+    git_tools = require("./common")
 
-var repos = [];
+var repos = []
 
 git_tools.getCurrentUser (function (current_user) {
   git_tools.getAllRepos (1, repos, function (repos) {
-    var myRepos = git_tools.getMyRepos (repos, current_user);
+    var myRepos = git_tools.getMyRepos (repos, current_user)
 
     git_tools.checkoutRepos (myRepos, 0, function () {
       console.log ("Done")
